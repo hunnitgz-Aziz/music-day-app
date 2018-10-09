0b5f56a9844003a44863c273f8e6125c9eaa57bb
Aziz Ramos <aziz.ramos@umusic.com>
2018-10-09 12:51:59 -0400

    adding GA to site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import AlbumComponent from './api.js';
 import Header from './containers/header';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+	ReactGA.initialize('UA-127143606-1');
+	ReactGA.pageview('/');
+}
 
 class App extends Component {
   render() {
